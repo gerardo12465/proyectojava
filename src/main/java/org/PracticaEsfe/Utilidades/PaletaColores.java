19aec18d89703b06615960f26c3d114482ce6d9e
Jazmin Eugenia Lue Duran <ACER@LAPTOP-FRMPB979>
2025-06-16 00:34:53 -0600

    commit

--- src/main/java/org/PracticaEsfe/Utilidades/PaletaColores.java
+++ src/main/java/org/PracticaEsfe/Utilidades/PaletaColores.java
@@ -2,15 +2,14 @@ package org.PracticaEsfe.Utilidades;
 
 import java.awt.Color;
 
-public class PaletaColores { // Nombre de la clase corregido
-    public static final Color PRIMARY_BROWN = new Color(101, 67, 33);
-    public static final Color LIGHT_BROWN = new Color(188, 152, 126);
-    public static final Color CREAM_WHITE = new Color(245, 245, 220);
-    public static final Color ACCENT_GOLD = new Color(212, 175, 55);
-    public static final Color TEXT_DARK = new Color(50, 50, 50);
-    public static final Color SOFT_BROWN = new Color(178, 150, 120);
-
-    public static final Color SEMI_TRANSPARENT_LIGHT_BROWN = new Color(LIGHT_BROWN.getRed(), LIGHT_BROWN.getGreen(), LIGHT_BROWN.getBlue(), 200);
-    public static final Color SEMI_TRANSPARENT_PRIMARY_BROWN = new Color(PRIMARY_BROWN.getRed(), PRIMARY_BROWN.getGreen(), PRIMARY_BROWN.getBlue(), 200);
+public class PaletaColores {
+    public static final Color PRIMARY_BROWN = new Color(101, 67, 33); // Café principal
+    public static final Color LIGHT_BROWN = new Color(188, 152, 126); // Café claro
+    public static final Color CREAM_WHITE = new Color(245, 245, 220); // Blanco crema
+    public static final Color ACCENT_GOLD = new Color(212, 175, 55); // Dorado de acento
+    public static final Color TEXT_DARK = new Color(50, 50, 50);     // Texto oscuro
+    // Café principal semi-transparente (200 es la opacidad, 0=transparente, 255=opaco)
+    public static final Color SEMI_TRANSPARENT_PRIMARY_BROWN = new Color(101, 67, 33, 200);
+    public static Color SEMI_TRANSPARENT_LIGHT_BROWN;
 }
 
